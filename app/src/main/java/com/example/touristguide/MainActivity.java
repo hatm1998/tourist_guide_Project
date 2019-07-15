8334dd72d9ee8de8898fe89d2be0d14eb6a34fa6
HatemDarwish Laptop <abudarwish1998@gmail.co>
2019-07-15 20:22:35 +0300

    hatem edit

--- app/src/main/java/com/example/touristguide/MainActivity.java
+++ app/src/main/java/com/example/touristguide/MainActivity.java
@@ -2,15 +2,33 @@ package com.example.touristguide;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.touristguide.authentication.Login;
+import com.google.firebase.auth.FirebaseAuth;
 
+public class MainActivity extends AppCompatActivity {
+    FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //
 
+        mAuth = FirebaseAuth.getInstance();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        if(mAuth.getCurrentUser() == null)
+        {
+            finish();
+            Intent intent = new Intent(getApplicationContext(),Login.class);
+            startActivity(intent);
+
+        }
     }
 }
---
+++ app/src/main/java/com/example/touristguide/authentication/Login.java
@@ -0,0 +1,61 @@
+package com.example.touristguide.authentication;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.touristguide.MainActivity;
+import com.example.touristguide.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class Login extends AppCompatActivity {
+
+    Button btn_login;
+    FirebaseAuth mAuth;
+    EditText txt_email, txt_pass;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        mAuth = FirebaseAuth.getInstance();
+        btn_login = findViewById(R.id.btn_login);
+
+        txt_email = findViewById(R.id.txt_email);
+        txt_pass = findViewById(R.id.txt_pass);
+
+
+        btn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                mAuth.signInWithEmailAndPassword(txt_email.getText().toString(), txt_pass.getText().toString())
+                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(getBaseContext(), "Done", Toast.LENGTH_LONG).show();
+                            finish();
+                            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                            startActivity(intent);
+                        } else {
+                            Toast.makeText(getBaseContext(), task.getException().getMessage(), Toast.LENGTH_LONG).show();
+                        }
+
+                    }
+                });
+            }
+        });
+    }
+}
