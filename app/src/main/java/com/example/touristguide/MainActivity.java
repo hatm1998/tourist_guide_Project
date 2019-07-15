ca9652515f0bd5184767dc5944f09400bb3c7a16
Ziad alkasaji <320160206009@STU.ttu.edu.jo>
2019-07-15 19:33:25 +0300

    ziad edit

--- app/src/main/java/com/example/touristguide/MainActivity.java
+++ app/src/main/java/com/example/touristguide/MainActivity.java
@@ -10,6 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        // hatem darwish 2018 / Ziad / Project
+        //  ziad la kasaji
     }
 }
