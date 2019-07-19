5122cc5e08df3eefc60c0ba2ff21f2ef78d7dc23
Ziad alkasaji <320160206009@STU.ttu.edu.jo>
2019-07-19 17:31:53 +0300

    ziad edit

---
+++ app/src/main/java/com/example/touristguide/Navigation_Drawer.java
@@ -0,0 +1,289 @@
+package com.example.touristguide;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.example.touristguide.Activity.Fragment_Activity;
+import com.example.touristguide.ShareItem.Add_new_post;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.navigation.NavigationView;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.ArrayList;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+import devlight.io.library.ntb.NavigationTabBar;
+
+public class Navigation_Drawer extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+
+    private FirebaseAuth mAuth;
+    private Button logout; // test button
+    private FirebaseFirestore firebaseFirestore;
+
+    private FloatingActionButton fab;
+    private NavigationTabBar navigationTabBar;
+    private CircleImageView Menu_profile_image;
+    private TextView menu_txt_username;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_navigation__drawer);
+        firebaseFirestore = FirebaseFirestore.getInstance();
+
+        Menu_profile_image = findViewById(R.id.menu_img_profile);
+        menu_txt_username = findViewById(R.id.menu_txt_username);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+        navigationView.setNavigationItemSelectedListener(this);
+//
+
+        View headerView = navigationView.getHeaderView(0);
+        TextView navUsername = (TextView) headerView.findViewById(R.id.menu_txt_username);
+        navUsername.setText("Your Text Here");
+
+
+
+
+        final String[] colors = getResources().getStringArray(R.array.colorful);
+        logout = findViewById(R.id.btn_logout);
+        navigationTabBar = findViewById(R.id.ntb);
+        mAuth = FirebaseAuth.getInstance();
+        fab = findViewById(R.id.fab_add);
+
+        // replace fragment Activity .
+        replacefragment(new Fragment_Activity());
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Navigation_Drawer.this, Add_new_post.class);
+                startActivity(intent);
+            }
+        });
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mAuth.signOut();
+                sendtologin();
+
+            }
+        });
+
+
+        ArrayList<NavigationTabBar.Model> models = new ArrayList<>();
+        models.add(
+                new NavigationTabBar.Model.Builder(
+                        getResources().getDrawable(R.drawable.ic_home),
+                        Color.parseColor(colors[0])
+                ).title("Home")
+
+                        .build()
+
+
+        );
+
+        models.add(
+                new NavigationTabBar.Model.Builder(
+                        getResources().getDrawable(R.drawable.ic_notifications),
+                        Color.parseColor(colors[1])
+                ).title("notifications")
+                        .build()
+        );
+        models.add(
+                new NavigationTabBar.Model.Builder(
+                        getResources().getDrawable(R.drawable.ic_category),
+                        Color.parseColor(colors[2])
+                ).title("Category")
+                        .build()
+        );
+        models.add(
+                new NavigationTabBar.Model.Builder(
+                        getResources().getDrawable(R.drawable.ic_action_name),
+                        Color.parseColor(colors[3])
+                ).title("Profile")
+                        .build()
+        );
+
+
+        navigationTabBar.setModels(models);
+//        navigationTabBar.setViewPager(viewPager, 1); // WHEN YOU USE VIEWPAGER
+
+        navigationTabBar.setTitleMode(NavigationTabBar.TitleMode.ACTIVE);
+
+        navigationTabBar.setBadgePosition(NavigationTabBar.BadgePosition.CENTER);
+        //navigationTabBar.setTypeface("fonts/custom_font.ttf");
+        navigationTabBar.setIsBadged(true);
+        navigationTabBar.setIsTitled(true);
+        navigationTabBar.setIsTinted(true);
+        navigationTabBar.setIsBadgeUseTypeface(true);
+        navigationTabBar.setBadgeTitleColor(Color.WHITE);
+        navigationTabBar.setIsSwiped(true);
+        navigationTabBar.setModelIndex(0);
+        navigationTabBar.setTitleSize(15);
+        navigationTabBar.setBehaviorEnabled(true);
+        navigationTabBar.setIconSizeFraction(0.5f);
+
+
+        navigationTabBar.setOnTabBarSelectedIndexListener(new NavigationTabBar.OnTabBarSelectedIndexListener() {
+            @Override
+            public void onStartTabSelected(NavigationTabBar.Model model, int index) {
+
+
+                return;
+
+            }
+
+
+            @Override
+            public void onEndTabSelected(NavigationTabBar.Model model, int index) {
+
+                switch (index) {
+                    case 0: {
+                        //replacefragment(home_fragment)
+                        replacefragment(new Fragment_Activity());
+                        break;
+                    }
+
+                    case 1: {
+                        //replacefragment(notifications_fragment);
+                        break;
+                    }
+                    case 2: {
+                        // replacefragment(category_fragment);
+                        break;
+                    }
+                    case 3: {
+                        // replacefragment(profile_fragment);
+                        break;
+                    }
+
+
+                }
+                return;
+            }
+        });
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+
+        getMenuInflater().inflate(R.menu.navigation__drawer, menu);
+
+
+
+        return true;
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_home) {
+            // Handle the camera action
+        } else if (id == R.id.nav_gallery) {
+
+        } else if (id == R.id.nav_slideshow) {
+
+        } else if (id == R.id.nav_tools) {
+
+        } else if (id == R.id.nav_share) {
+
+        }
+
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        if (mAuth.getCurrentUser() == null) {
+
+            sendtologin();
+        }
+
+    }
+
+
+    public void replacefragment(Fragment fragment) {
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.frameLayout, fragment);
+
+        fragmentTransaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN);
+        fragmentTransaction.commit();
+
+    }
+
+    private void sendtologin() {
+
+        finish();
+        Intent intent = new Intent(this, Contact_page.class);
+        startActivity(intent);
+    }
+}
