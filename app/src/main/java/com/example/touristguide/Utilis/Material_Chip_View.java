d1e4715b1299bcd5c8aa18fb64d86f7c2bd57d77
Ziad alkasaji <320160206009@STU.ttu.edu.jo>
2019-08-05 22:51:23 +0300

    ziad

--- app/src/main/java/com/example/touristguide/Utilis/Material_Chip_View.java
+++ app/src/main/java/com/example/touristguide/Utilis/Material_Chip_View.java
@@ -1,4 +1,4 @@
-package com.example.touristguide;
+package com.example.touristguide.Utilis;
 
 import android.graphics.Color;
 import android.os.Bundle;
@@ -14,6 +14,7 @@ import android.widget.ScrollView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.touristguide.R;
 import com.nex3z.flowlayout.FlowLayout;
 import com.robertlevonyan.views.chip.Chip;
 
